dcd5101daa0c487dc0ced457af1cec65780276ae
Michael <michael712514@gmail.com>
2022-03-18 15:23:45 -0400

    Initial Commit

---
+++ H4X0R/Views/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  H4X0R
+//
+//  Created by Michael Abrams on 8/30/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @ObservedObject var networkManager = NetworkManager()
+    
+    var body: some View {
+        NavigationView {
+            List(networkManager.posts) { post in
+                NavigationLink(
+                    destination: DetailView(url: post.url)) {
+                    HStack {
+                        Text(String(post.points))
+                        Text(post.title)
+                    }
+                }
+                
+            }
+            .navigationBarTitle("H4X0R NEWS")
+        }
+        .onAppear(perform: {
+            self.networkManager.fetchData()
+        })
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
+
+
+
